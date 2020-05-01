ecef9dfca1bb6dd5d68dac18d3c76dab771ac49a
CANewbould <60323628+CANewbould@users.noreply.github.com>
2020-05-01 15:53:10 +0100

    Updated batch files
    Versions of eu3.bat

--- euphoria321/demos/std/eu3.bat
+++ euphoria321/demos/std/eu3.bat
@@ -5,16 +5,17 @@ REM Version 3.2.1.1: 2020/05/01; C A Newbould - revised for GitHub folder struct
 REM Set the environment for this installation of Euphoria (v3.2.1)
 REM The interpreter is RDS3.1.1
 REM The installation is 32-bit
+REM This is the "eu.cfg" equivalent for executing the demos relating to the std libraries
 REM
 
 ECHO Euphoria Version 3.2.1: Core RDS Euphoria Interpreter (32-bit) with "std" libraries
 
-SET EUDIR=..\..\euphoria311
-SET PATH=%EUDIR%\bin;%PATH%;\dll;
-SET EUINC=..\include
+SET EUDIR=..\..\..\euphoria311
+SET PATH=%EUDIR%\bin;%PATH%;..\..\dll;
+SET EUINC=..\..\include
 
-::ECHO EUDIR = %EUDIR%
-::ECHO EUINC = %EUINC%
-ECHO Command is %EUDIR%\bin\exw %1%
+:: ECHO EUDIR = %EUDIR%
+:: ECHO EUINC = %EUINC%
+:: ECHO Command is %EUDIR%\bin\exw %1%
 
 %EUDIR%\bin\exw %1
